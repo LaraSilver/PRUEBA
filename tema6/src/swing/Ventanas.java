f07cf82ac32ab4b170bd7cfda5998864394a43d8
LaraSilver <49073375+LaraSilver@users.noreply.github.com>
2019-04-08 16:53:00 +0200

    Update Ventanas.java

--- tema6/src/swing/Ventanas.java
+++ tema6/src/swing/Ventanas.java
@@ -14,6 +14,7 @@ public class Ventanas {
 		VentanaCentrada vCentro = new VentanaCentrada();
 		
 		System.out.println("CAMBIO PARA VER GITHUB");
+		System.out.p`rintln("Cambio DESDE el editor github");
 	}
 
 	 static class VentanaA extends JFrame {
