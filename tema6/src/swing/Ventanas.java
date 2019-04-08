98f177a8aca07cac0856f7a5cd8571d6534f0039
LaraSilver <sasrlarafdez@gmail.com>
2019-04-08 16:15:12 +0200

    Nuevo commit prueba

---
+++ tema6/src/swing/Ventanas.java
@@ -0,0 +1,66 @@
+package swing;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+import javax.swing.*;
+
+
+public class Ventanas {
+	public static void main(String[] args) {
+		VentanaA ventana = new VentanaA();
+		VentanaCentrada vCentro = new VentanaCentrada();
+	}
+
+	 static class VentanaA extends JFrame {
+		public VentanaA() {
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			setVisible(true);
+			
+			Toolkit pantallita = Toolkit.getDefaultToolkit();
+			Dimension tamanio = pantallita.getScreenSize();
+			int ancho = tamanio.width;
+			int alto = tamanio.height;
+			setSize(ancho, 200);
+			setLocation(0, alto / 4);
+			setTitle("Fatty ventana");
+			Image icono = pantallita
+					.getImage("C:\\Users\\jorge\\Desktop\\programas\\ventana_ancho.jpg");
+			setIconImage(icono);
+			
+		}
+	}
+
+	 static class VentanaCentrada extends JFrame {
+		public VentanaCentrada() {
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			setVisible(true);
+			
+			Toolkit pCentro = Toolkit.getDefaultToolkit();
+			
+			Dimension tamanio = pCentro.getScreenSize();
+			int ancho = tamanio.width;
+			int alto = tamanio.height;
+			setSize(300,300);
+			setLocation(ancho/2, alto /2);
+			setLocationRelativeTo(null);
+			Image icono = pCentro
+					.getImage("C:\\Users\\jorge\\Desktop\\programas\\ventana_centro.jpg");
+			setIconImage(icono);
+			Lamina lam = new Lamina();
+			add(lam);
+
+		}
+	}
+	
+	static class Lamina extends JPanel{
+		public void paintComponent(Graphics g){
+			g.drawString("Holiis, caracoliiiss", 150, 150);
+			g.drawRect(50, 50, 100, 100);
+		}
+		
+	}
+}
+
